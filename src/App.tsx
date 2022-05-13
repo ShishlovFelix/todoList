ab02e4789f45dee823dfb81caa985b4aec49931e
ShishlovFelix <81853629+ShishlovFelix@users.noreply.github.com>
2022-05-13 23:48:50 +0300

    -add functional

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,113 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import {pink} from "@mui/material/colors";
+import {Box, Button, Checkbox, Grid, TextField, Typography} from "@mui/material";
+import BookmarkBorderIcon from "@mui/icons-material/BookmarkBorder";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
 
-function App() {
+interface IList {
+  value: string;
+}
+
+const App= () => {
+  const [list, setList] = useState<IList[]>([]);
+  const [text, setText] = useState<string>("");
+  const addButtonClick = () => {
+    if (text === "") {
+      return alert("no Text");
+    }
+    setText("");
+    setList((pevList) => [...pevList, { value: text }]);
+  };
+
+  const ListGrid: React.FC<{ value: string; index: number }> = (props) => {
+    const { value, index } = props;
+    const [importantValue, setImportantValue] = useState<boolean>(false);
+    const [doneValue, setDoneValue] = useState<boolean>(false);
+
+    const delButtonClick = () => {
+      setList((pevList) => pevList.filter((_, eIndex) => eIndex !== index));
+    };
+
+    return (
+        <Box>
+          <Box sx={{ display: "flex", justifyContent: "end" }}>
+            <Typography
+                sx={{
+                  color: importantValue ? "coral" : "white",
+                  textDecoration: doneValue ? "line-through" : "none",
+                }}
+                variant="h3"
+            >
+              {value}
+            </Typography>
+            <Button
+                sx={{ ml: 2, padding: "13px 23px" }}
+                variant="outlined"
+                onClick={delButtonClick}
+            >
+              Del
+            </Button>
+            <Checkbox
+                icon={<BookmarkBorderIcon />}
+                checkedIcon={<BookmarkIcon />}
+                checked={importantValue}
+                onChange={(e) => {
+                  setImportantValue(e.target.checked);
+                }}
+            />
+            <Checkbox
+                checked={doneValue}
+                sx={{
+                  color: pink[800],
+                  "&.Mui-checked": {
+                    color: pink[600],
+                  },
+                }}
+                onChange={(e) => {
+                  setDoneValue(e.target.checked);
+                }}
+            />
+          </Box>
+        </Box>
+    );
+  };
+  const listElements = list.map((element, index) => {
+    const { value } = element;
+    return <ListGrid value={value} index={index} />;
+  });
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Grid container>
+
+        <Grid xs={12} sx={{ display: "flex", justifyContent: "center" }}>
+          <Box>
+            <Box>
+              <Typography
+                  sx={{ mt: 10, mb: 6 }}
+                  variant="h2"
+                  component="div"
+                  color="coral"
+              >
+                ToDoList
+              </Typography>
+              <TextField
+                  value={text}
+                  id="outlined-basic"
+                  label="Write To Do"
+                  variant="outlined"
+                  onChange={(e) => setText(e.target.value)}
+              />
+              <Button
+                  variant="contained"
+                  sx={{ ml: 1, height: "50px" }}
+                  onClick={addButtonClick}
+              >
+                Add
+              </Button>
+            </Box>
+            <Box sx={{ mt: 8 }}>{listElements}</Box>
+          </Box>
+        </Grid>
+      </Grid>
   );
 }
 
